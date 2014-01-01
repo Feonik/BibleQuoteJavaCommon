26c450e4bdd74d2fc2c99701ebf0b74f16bd6c61
Feonik <nikita_k@ur.net>
2014-01-01 21:50:38 +0100

    Initial commit (3)

--- BibleQuoteJavaCommon/src/Common.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Nikita K.
- * Date: 01.01.14
- * Time: 21:41
- * To change this template use File | Settings | File Templates.
- */
-public class Common {
-}
